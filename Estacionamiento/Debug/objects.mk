d65473f7241d8e6ffc808ec0572eb529456cbd3c
mateososter <31116480+mateososter@users.noreply.github.com>
2017-11-11 18:44:02 -0300

    Pruebas y mejoras
    Anda hasta la parte en que tiene que modificar el mensaje del display. Se chotea cuando quiere tocar un registro del timer1. Lee bien la tarjeta. Revisar tema del delay de la libreria lcd. Esta al reves el orden de prioridad de los lugares.

---
+++ Estacionamiento/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lCMSIS_CORE_LPC17xx -llpc_board_nxp_lpcxpresso_1769 -llpc_chip_175x_6x
+
---
+++ Estacionamiento/Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/Estacionamiento.c \
+../src/cr_startup_lpc175x_6x.c \
+../src/crp.c \
+../src/sysinit.c 
+
+OBJS += \
+./src/Estacionamiento.o \
+./src/cr_startup_lpc175x_6x.o \
+./src/crp.o \
+./src/sysinit.o 
+
+C_DEPS += \
+./src/Estacionamiento.d \
+./src/cr_startup_lpc175x_6x.d \
+./src/crp.d \
+./src/sysinit.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M3 -D__USE_LPCOPEN -D__LPC17XX__ -D__REDLIB__ -I"C:\Users\vargjere\Documents\MCUXpressoIDE_10.0.2_411\workspace\CMSIS_CORE_LPC17xx\inc" -I"C:\Users\vargjere\Documents\MCUXpressoIDE_10.0.2_411\workspace\Estacionamiento\inc" -I"C:\Users\vargjere\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_board_nxp_lpcxpresso_1769\inc" -I"C:\Users\vargjere\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_175x_6x\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
